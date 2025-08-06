4119ba8d9260f114ed929cad93b2876975a4f999
Michal Harrison <mlh1477@gmail.com>
2025-08-06 13:33:17 -0400

    4.45 Date Picker with filters using matDatepickerFilter

--- src/app/pages/contact-us-form/contact-us-form.component.ts
+++ src/app/pages/contact-us-form/contact-us-form.component.ts
@@ -19,6 +19,12 @@ import { provideNativeDateAdapter } from '@angular/material/core';
 export class ContactUsFormComponent {
   form: FormGroup;
 
+  weekendFilter = (d: Date | null): boolean => {
+    const day = (d || new Date()).getDay();
+    // Prevent Saturday and Sunday from being selected.
+    return day !== 0 && day !== 6;
+  };
+
   constructor(private fb: FormBuilder) {
     this.form = this.fb.group({
       name: ['', Validators.required],
