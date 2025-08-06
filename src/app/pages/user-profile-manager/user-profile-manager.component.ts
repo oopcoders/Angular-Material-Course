f7f545a9f2f088ccd1292ac4709c5190db1b5375
Michal Harrison <mlh1477@gmail.com>
2025-08-06 13:42:11 -0400

    5.49 User Profile Card with mat-card

--- src/app/pages/user-profile-manager/user-profile-manager.component.ts
+++ src/app/pages/user-profile-manager/user-profile-manager.component.ts
@@ -1,8 +1,10 @@
 import { Component, inject } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-user-profile-manager',
-  imports: [],
+  imports: [MatButtonModule, MatCardModule],
   templateUrl: './user-profile-manager.component.html',
   styleUrl: './user-profile-manager.component.scss'
 })
