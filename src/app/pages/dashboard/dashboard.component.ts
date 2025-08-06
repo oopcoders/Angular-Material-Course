e04cb1b07f9b391760f7c18b228e1c61284932c2
Michal Harrison <mlh1477@gmail.com>
2025-08-06 10:58:22 -0400

    3.35 Responsive Layout with Breakpoints & CDK Layout

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -2,13 +2,22 @@ import { Component, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatButtonToggleModule } from '@angular/material/button-toggle';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { map } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [MatButtonModule, MatButtonToggleModule, MatGridListModule],
+  imports: [MatButtonModule, MatButtonToggleModule, MatGridListModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  private breakpointObserver = inject(BreakpointObserver);
+
+  readonly col$ = this.breakpointObserver.observe([Breakpoints.Small, Breakpoints.XSmall, Breakpoints.Medium]).pipe(
+    map(state => state.matches ? 1 : 2)
+  );
 
 }
